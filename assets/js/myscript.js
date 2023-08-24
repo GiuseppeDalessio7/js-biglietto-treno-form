2e98cbd4f020ab04d0d21305f71721d9c2977962
GiuseppeDalessio7 <g.dalessio03gmail.com>
2023-08-24 17:42:39 +0200

    finish  MILESTONE 1

--- assets/js/myscript.js
+++ assets/js/myscript.js
@@ -6,32 +6,31 @@ va applicato uno sconto del 20% per i minorenni
 va applicato uno sconto del 40% per gli over 65*/
 
 
-//nome dell'utente
-const userName = (prompt('Quale è il tuo nome?'));
-// console.log('Il tuo nome è:' + userName);
+document.getElementById('genera').addEventListener('click', function () {
+    //nome dell'utente 
+    const userName = document.getElementById('name').value;
+    console.log('Il tuo nome è:' + userName);
+    //Il numero di chilometri da percorrere
+    const km = document.getElementById('klm').value;
+    console.log('I chilometri da percorrere sono:' + km);
+    //Età del passeggero
+    const age = document.getElementById('ageUser').value;
+    console.log('La tua età è:' + age);
+    //il prezzo del biglietto è definito in base ai km (0.21 € al km)
+    const priceTicket = 0.21;
+    
+    let totalPrice = (km * priceTicket);
+    console.log('Il costo del biglietto è:€ ' + totalPrice.toFixed(2));
+    
+    //va applicato uno sconto del 20% per i minorenni
+    if(age <= 18 ) {
+        totalPrice = totalPrice - (totalPrice * 0.2)
+        console.log('Il prezzo da pagare dopo uno sconto del 20% è: €'+ totalPrice.toFixed(2));
+    } //va applicato uno sconto del 40% per gli over 65*/ 
+        else if (age >= 65) {
+        totalPrice = totalPrice - (totalPrice * 0.4)
+        console.log('Il prezzo da pagare dopo uno sconto del 40% è: €'+ totalPrice.toFixed(2));
+    }
+    });
 
-document.getElementById('name').innerHTML ='Il tuo nome è:' + userName;
-
-
-
-//Il numero di chilometri da percorrere
-const km = Number(prompt('Quanti chilometri devi percorrere?'));
-console.log('I chilometri da percorrere sono:' + km);
-//Età del passeggero
-const age = Number(prompt('Scrivi qui i tuoi anni'));
-console.log('La tua età è:' + age);
-//il prezzo del biglietto è definito in base ai km (0.21 € al km)
-const priceTicket = 0.21;
-
-let totalPrice = (km * priceTicket);
-console.log('Il costo del biglietto è:€ ' + totalPrice.toFixed(2));
-
-//va applicato uno sconto del 20% per i minorenni
-if(age <= 18 ) {
-    totalPrice = totalPrice - (totalPrice * 0.2)
-    console.log('Il prezzo da pagare dopo uno sconto del 20% è: €'+ totalPrice.toFixed(2));
-} //va applicato uno sconto del 40% per gli over 65*/ 
-    else if (age >= 65) {
-    totalPrice = totalPrice - (totalPrice * 0.4)
-    console.log('Il prezzo da pagare dopo uno sconto del 40% è: €'+ totalPrice.toFixed(2));
-}
+    document.getElementById("text_name").innerHTML = Username;
